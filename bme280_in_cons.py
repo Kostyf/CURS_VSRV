b12c5ee77b5460c2e24ea70a764bff84b5fd4082
Kostyf <70743529+Kostyf@users.noreply.github.com>
2021-12-18 19:52:15 +0300

    Add files via upload

---
+++ bme280_in_cons.py
@@ -0,0 +1,21 @@
+
+#Импортирую датчики для доступа к ним
+from smbus import SMBus
+from bme280 import BME280
+
+#инициализация датчика BME280
+bus = SMBus(1)
+bme280 = BME280(i2c_dev=bus)
+
+#считываю данные в первый раз, как мусорные
+temperature = bme280.get_temperature()
+pressure = bme280.get_pressure()
+humidity = bme280.get_humidity()
+time.sleep(1)	
+
+while True:
+    temperature = bme280.get_temperature()
+    pressure = bme280.get_pressure()
+    humidity = bme280.get_humidity()
+    print('{:05.2f}*C {:05.2f}hPa {:05.2f}%'.format(temperature, pressure, humidity))
+    time.sleep(1)
